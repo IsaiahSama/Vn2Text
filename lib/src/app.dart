c56e1754edd643885e50245cf7ee6893ea22c9c6
IsaiahSama <zeldaplayergl@gmail.com>
2024-11-07 16:01:03 -0400

    Added Riverpod for state management

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:voice_note_to_text/src/filepicker.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,20 +11,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('Voice Note to Text')),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('Voice Note to Text')),
+        ),
+        body: const MyHomePage(),
       ),
-      body: const MyHomePage(),
-    ));
+    );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+final fileProvider = StateProvider<File?>((ref) => null);
+final errorProvider = StateProvider<String>((ref) => "");
+
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Center(
       child: Column(
         children: [
@@ -31,10 +36,14 @@ class MyHomePage extends StatelessWidget {
               "https://th.bing.com/th/id/OIG2.DY4RCeB1fLi1yzfYWYzq?pid=ImgGn",
               height: 100,
               width: 100),
+          Text(ref.watch(errorProvider).toString(),
+              style: const TextStyle(
+                color: Colors.red,
+              )),
           Padding(
             padding: const EdgeInsets.all(10),
             child: ElevatedButton(
-              onPressed: uploadVoiceNote,
+              onPressed: () => uploadVoiceNote(ref),
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.blue,
                 foregroundColor: Colors.white,
@@ -45,7 +54,7 @@ class MyHomePage extends StatelessWidget {
           Padding(
               padding: const EdgeInsets.all(10),
               child: ElevatedButton(
-                onPressed: transcribe,
+                onPressed: () => transcribe(ref),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.green,
                   foregroundColor: Colors.white,
@@ -58,9 +67,7 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-
-void uploadVoiceNote() async{
-
+void uploadVoiceNote(WidgetRef ref) async {
   print("Upload Voice Note");
 
   // Select a file
@@ -69,18 +76,19 @@ void uploadVoiceNote() async{
   // If the file doesn't exist, notify user and return.
   if (result == null) {
     // Display Error message
+    ref.read(errorProvider.notifier).state = "No file selected";
     return;
   }
 
   // Temporarily store the file for transcription.
 
   File file = File(result.files.single.path!);
+
   // Store file in Riverpod!
-  
+  ref.read(fileProvider.notifier).state = file;
 }
 
-void transcribe() {
-
+void transcribe(WidgetRef ref) async {
   print("Transcribe!");
 
   // Transcribe the file using my API
@@ -89,4 +97,3 @@ void transcribe() {
 
   // If there's an error, display it.
 }
-
